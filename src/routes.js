10e44392e133dd61862b7ad29505eeecedc0ef06
hianmateus <hianmateus1@gmail.com>
2024-03-29 10:14:44 -0300

    codigo bugado

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+const routes = new Router();
 
 routes.get('/', (request, response) =>
   response.status(201).json({ message: 'Eae Brow' }),
-)
+);
 
-export default routes
+export default routes;
