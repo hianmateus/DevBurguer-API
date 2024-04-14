199d265296ccf1eb86ac69a0bd988bfc043fe7a8
hianmateus <hianmateus1@gmail.com>
2024-04-14 19:40:49 -0300

    aula 13

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize'
+
+import configdatabase from '../config/database'
+
+import User from '../app/models/User'
+
+const models = [User]
+
+class Database {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.connection = new Sequelize(configdatabase)
+    models.map((model) => model.init(this.connection))
+  }
+}
+
+export default new Database()
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) =>
-  response.status(201).json({ message: 'Eae Brow' }),
-)
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Hian',
+    email: 'hianmateus@gmail.com',
+    password_hash: 'hian123',
+  })
+
+  return response.status(201).json(user)
+})
 
 export default routes
