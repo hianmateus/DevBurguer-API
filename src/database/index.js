4e1ff65fdeeeb6fb07b6d71701ba395cf50fa0e7
hianmateus <hianmateus1@gmail.com>
2024-05-20 21:36:06 -0300

    devburguer al8Finalizada

--- src/database/index.js
+++ src/database/index.js
@@ -15,7 +15,7 @@ class Database {
 
   init() {
     this.connection = new Sequelize(configdatabase)
-    models.map((model) => model.init(this.connection))
+    models.map((model) => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models))
   }
 }
 
